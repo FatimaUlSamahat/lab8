8421292c6f36c5e18dee6643325d0043545c1031
FatimaUlSamahat <27100064@lums.edu.pk>
2026-03-17 11:43:55 +0500

    Implement hasCity

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -17,5 +17,9 @@ public class CustomList {
     
 
     // Will be implemented later using TDD (leave blank for now)
-    // public boolean hasCity(City city) { ... }
+     public boolean hasCity(City city) {
+        return cities.contains(city);
+
+
+    }
 }
