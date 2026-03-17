4dbf18dd6c8adf89918af9cf43c4ee5418215ae4
FatimaUlSamahat <27100064@lums.edu.pk>
2026-03-17 11:40:55 +0500

    Add failing test for hasCity

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,5 +3,13 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
+    @Test
+    public void testhascit()
+    {
+        CustomList li = new CustomList();
+        City calg = new City("Calgary","AB");
+        li.addCity(calg);
+        assertTrue(li.hasCity(calg));
+    }
 
 }
